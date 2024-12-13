346dc720197f1fd0631b7c580b9cf176ee8882ea
soogguang <susuthelove@naver.com>
2024-12-13 13:17:40 +0900

    Update 과정.py

--- Sleep_health_and_lifestyle/pages/과정.py
+++ Sleep_health_and_lifestyle/pages/과정.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 # 이미지 파일 경로
-image_path = "Sleep_health_and_lifestyle/pages/Notebooks_page.jpg"  # 실제 이미지 파일 경로를 입력하세요.
+image_path = "Sleep_health_and_lifestyle/pages/Notebooks_page.jpg" 
 
 # 이미지 표시
-st.image(image_path, caption="Smart Bug Trap 이미지", use_container_width=True)
+st.image(image_path, caption="colab 화면 이미지", use_container_width=True)

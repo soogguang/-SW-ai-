b2f145a393a73feee013c86a97856cb49b603e28
soogguang <susuthelove@naver.com>
2024-12-13 12:43:14 +0900

    Create 과정.py

---
+++ Sleep_health_and_lifestyle/pages/과정.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import streamlit as st
+
+# 스크린샷 이미지 파일을 대시보드에 표시
+image = Image.open("Sleep_health_and_lifestyle/Notebooks.pdf")
+st.image(image, caption="숙면을 위한 팁", use_column_width=True)

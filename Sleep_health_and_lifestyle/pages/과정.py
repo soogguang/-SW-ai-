6a62c12a56d5158755e0df1f2239385504759c54
soogguang <susuthelove@naver.com>
2024-12-13 13:46:34 +0900

    Update 과정.py

--- Sleep_health_and_lifestyle/pages/과정.py
+++ Sleep_health_and_lifestyle/pages/과정.py
@@ -1,6 +1,7 @@
 import streamlit as st
-st.title("사용하기위한 데이터셋을 만드는 과정")
 st.set_page_config(layout="wide")
+st.title("사용하기위한 데이터셋을 만드는 과정")
+
 
 # 이미지 파일 경로
 image_path = "Sleep_health_and_lifestyle/pages/Notebooks_page.jpg" 

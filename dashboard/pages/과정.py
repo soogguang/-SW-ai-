32a20c77dcab74d8dd03f76b679ad85e498b3158
soogguang <susuthelove@naver.com>
2026-01-14 22:16:23 +0900

    Change image file format from JPG to PNG

--- dashboard/pages/과정.py
+++ dashboard/pages/과정.py
@@ -7,7 +7,7 @@ image_path1 = "dashboard/pages/Notebooks_page.jpg"  # 실제 이미지 파일 �
 st.image(image_path1, caption="데이터 처리 과정")
 
 # 이미지 파일 경로
-image_path2 = "dashboard/pages/model_training_page.jpg"  # 실제 이미지 파일 경로를 입력하세요.
+image_path2 = "dashboard/pages/model_training_page.png"  # 실제 이미지 파일 경로를 입력하세요.
 
 # 이미지 표시
 st.image(image_path2, caption="모델 학습 과정")
